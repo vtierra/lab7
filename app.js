be4f674f4a26dee1936db0aaabefb182b0384dcd
Vincent Tierra <vtierra@ucsd.edu>
2018-02-22 10:47:45 -0800

    lab7

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var project = require('./routes/project');
+
+// var viewAlt = false;
 // Example route
 // var user = require('./routes/user');
 
@@ -37,6 +39,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/viewAlt', index.viewAlt);
 app.get('/project/:id', project.view);
 // Example route
 // app.get('/users', user.list);
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(request, response){
+	projects['viewAlt'] = true;
   	response.render('index', projects);
+};
+
+exports.viewAlt = function(req, res) {
+	projects['viewAlt'] = false;
+	res.render('index', projects);
 };
